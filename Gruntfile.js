2e52b67123b1f160845110059c9bfce367151131
andrewbelcher <andrewbelcher@655282.no-reply.drupal.org>
2017-06-02 04:44:12 -0400

    Isssue #3 by andrewbelcher: Start switch to bootstrap.

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      dist: {
+        options: {
+          loadPath: [
+            'node_modules/bootstrap/scss'
+          ]
+        },
+        files: [
+          {
+            expand: true,
+            cwd: 'scss',
+            src: '**/*.scss',
+            dest: 'css',
+            ext: '.css'
+          }
+        ]
+      }
+    },
+    autoprefixer:{
+      dist:{
+        options: {
+          map: true
+        },
+        files:[
+          {
+            expand: true,
+            cwd: 'css',
+            src: '**/*.css',
+            dest: 'css',
+            ext: '.css'
+          }
+        ]
+      }
+    },
+    watch: {
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass', 'autoprefixer']
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.registerTask('default',['watch']);
+  grunt.registerTask('once',['sass', 'autoprefixer']);
+}
